d73bf3eed5b7417bfbf8cd7cc50ff998469d7291
Tedesqui <87279924+Tedesqui@users.noreply.github.com>
2025-07-24 23:18:43 -0300

    Add files via upload

---
+++ public/assets/service-worker.js
@@ -0,0 +1,11 @@
+// public/service-worker.js
+self.addEventListener('push', event => {
+    const data = event.data.json();
+    const options = {
+        body: data.body,
+        icon: data.icon,
+    };
+    event.waitUntil(
+        self.registration.showNotification(data.title, options)
+    );
+});
