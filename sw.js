29aa59dbf456a4043350c14899bcf831356c8a5e
Tedesqui <87279924+Tedesqui@users.noreply.github.com>
2025-07-25 01:29:14 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ self.addEventListener('message', event => {
   if (event.data === 'scheduleNotification') {
     setTimeout(() => {
       self.registration.showNotification('𝐇𝐎𝐋𝐎𝐆𝐑𝐀𝐌 𝐂𝐎𝐈𝐍', {
-        body: '𝗟𝗶𝗳𝗲 𝗶𝘀 𝗺𝗼𝗿𝗲 𝗳𝘂𝗻 𝘄𝗶𝘁𝗵 𝗖𝗥𝗬𝗣𝗧𝗢. 𝗬𝗼𝘂'𝗿𝗲 𝗮𝗹𝘄𝗮𝘆𝘀 𝘄𝗲𝗹𝗰𝗼𝗺𝗲.',
+        body: '𝗟𝗶𝗳𝗲 𝗶𝘀 𝗺𝗼𝗿𝗲 𝗳𝘂𝗻 𝘄𝗶𝘁𝗵 𝗖𝗥𝗬𝗣𝗧𝗢. 𝗬𝗼𝘂 ar𝗲 𝗮𝗹𝘄𝗮𝘆𝘀 𝘄𝗲𝗹𝗰𝗼𝗺𝗲.',
         icon: 'videos/hannah.png', // ícone pequeno
         image: 'videos/hannah.png', // ícone grande (visível em alguns navegadores)
         badge: 'videos/hannah.png', // ícone da notificação
