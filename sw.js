d51b9c339c9cc1ec0e5d22f571967cd307ce1e16
Tedesqui <87279924+Tedesqui@users.noreply.github.com>
2025-07-25 00:53:10 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,19 +1,14 @@
-// Dentro de sw.js
+// sw.js
+self.addEventListener('install', () => self.skipWaiting());
+self.addEventListener('activate', () => self.clients.claim());
 
-// Escuta por mensagens vindas da página principal
 self.addEventListener('message', event => {
-    // Verifica se a mensagem é para agendar a notificação
-    if (event.data && event.data.type === 'schedule-notification') {
-
-        // Define o tempo de espera (1 minuto = 60000 milissegundos)
-        const delay = 10000;
-
-        setTimeout(() => {
-            // Exibe a notificação após o tempo de espera
-            self.registration.showNotification('𝐇𝐎𝐋𝐎𝐆𝐑𝐀𝐌 𝐂𝐎𝐈𝐍', {
-                body: '𝗔𝗧𝗧𝗘𝗡𝗧𝗜𝗢𝗡: 𝗡𝗲𝘄 𝗰𝗿𝘆𝗽𝘁𝗼!!! 𝗧𝗮𝗸𝗲 𝘁𝗵𝗲 𝗰𝗵𝗮𝗻𝗰𝗲 𝘁𝗼 𝗺𝗮𝗸𝗲 𝗮 𝗹𝗼𝘁 𝗼𝗳 𝗺𝗼𝗻𝗲𝘆 𝗮𝗻𝗱 𝗯𝘂𝘆 𝗻𝗼𝘄!!!',
-                icon: 'videos/hannah.png' // Opcional: ícone para a notificação
-            });
-        }, delay);
-    }
+  if (event.data === 'scheduleNotification') {
+    setTimeout(() => {
+      self.registration.showNotification('𝐇𝐎𝐋𝐎𝐆𝐑𝐀𝐌 𝐂𝐎𝐈𝐍', {
+        body: '𝗔𝗧𝗧𝗘𝗡𝗧𝗜𝗢𝗡: 𝗡𝗲𝘄 𝗰𝗿𝘆𝗽𝘁𝗼!!! 𝗧𝗮𝗸𝗲 𝘁𝗵𝗲 𝗰𝗵𝗮𝗻𝗰𝗲 𝘁𝗼 𝗺𝗮𝗸𝗲 𝗮 𝗹𝗼𝘁 𝗼𝗳 𝗺𝗼𝗻𝗲𝘆 𝗮𝗻𝗱 𝗯𝘂𝘆 𝗻𝗼𝘄!!!',
+        icon: 'videos/hannah.png', // opcional
+      });
+    }, 10000); // 10 segundos
+  }
 });
